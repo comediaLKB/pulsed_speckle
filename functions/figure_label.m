c3d3beed43da62d3fe4c5e9c5b829af5c2bf64d3
Bernhard Rauer <bernhard.rauer@gmail.com>
2020-04-07 18:19:39 +0200

    Modified figure label function to support custom positioning and font size changes. Changed labels to bold Arial. Made dummy axis smaller.

--- functions/figure_label.m
+++ functions/figure_label.m
@@ -1,4 +1,4 @@
-function [axes_children] = figure_label(axes_parent,number)
+function [axes_children] = figure_label(axes_parent, number, pos_offset, font_size)
 
 %This code anables to put some labeling text to figures. It takes an axis
 %in input and add another one, invisible and with some text, nearby it.
@@ -6,10 +6,30 @@ function [axes_children] = figure_label(axes_parent,number)
 % get axis position
 Pos = get(axes_parent,'Position');
 
+% chose default font size from figure height
+if nargin < 4
+    font_size = 15+2*Pos(4);
+end
+
+% text position - default upper edge of the plot
+if nargin < 3
+    pos_offset = [0 0];
+end
+text_pos(1) = Pos(1) + pos_offset(1);
+text_pos(2) = Pos(2) + Pos(4) + pos_offset(2);
+axis_width = 0.3;
+
 % create an invisible axis a bit moved from first one
-axes_children = axes('units', 'centimeters', 'position', [Pos(1)-Pos(3)/4 Pos(2)+Pos(4)+0.25 1 1])
+%axes_children = axes('units', 'centimeters', 'position', [Pos(1)-Pos(3)/4 Pos(2)+Pos(4)+0.25 0.3 0.3]);
+axes_children = axes('units', 'centimeters', 'position', [text_pos(1) text_pos(2) axis_width axis_width]);
 set(axes_children, 'Visible', 'off')
+set(axes_children, 'XTickLabel', [],'YTickLabel', [])
 
 % add some text in it
-text(0.25,0.25,number,'Parent',axes_children,'FontWeight','bold','FontSize',15+2*Pos(4));
+handle_text = text(0,0,number,'Parent',axes_children,'FontSize', font_size);
+set(handle_text,'Interpreter',  'Tex',...
+                'FontName',     'Arial',...
+                'FontWeight',   'bold',...
+                'FontSize',     font_size)
+
 end
