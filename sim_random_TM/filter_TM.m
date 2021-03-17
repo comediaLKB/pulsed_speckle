c590b32b2bc4476ab0cd1df68c70d8647a1097bd
Bernhard Rauer <bernhard.rauer@gmail.com>
2021-03-17 11:13:51 +0100

    ring width adjustable, added single k pixel filter

--- sim_random_TM/filter_TM.m
+++ sim_random_TM/filter_TM.m
@@ -1,4 +1,8 @@
-function [TM_filt, TM_pic_fft] = filter_TM(TM, filter_width, filter_flag)
+function [TM_filt, TM_pic_fft] = filter_TM(TM, filter_width, filter_flag, ring_width)
+
+if nargin < 4
+    ring_width = 1;
+end
 
 [n_ccd, n_slm] = size(TM);
 
@@ -18,12 +22,12 @@ for idx_slm = 1:n_slm
         TM_pic = fftshift( ifft2( smear_kernal_k .* fft2(TM_pic) ) );        
     elseif filter_flag == 2 % ring
         smear_kernal_k = zeros(size(TM_pic));
-        k_min = filter_width - 1;
-        k_max = filter_width + 1;
-        smear_kernal_k((sqrt(KX.^2 + KY.^2) > k_min) & (sqrt(KX.^2 + KY.^2) < k_max)) = 1;
+        k_min = filter_width - ring_width;
+        k_max = filter_width;
+        smear_kernal_k((sqrt(KX.^2 + KY.^2) >= k_min) & (sqrt(KX.^2 + KY.^2) <= k_max)) = 1;
         smear_kernal_k = smear_kernal_k / sum(sum(smear_kernal_k));
         smear_kernal_k = fftshift(smear_kernal_k);
-        TM_pic = fftshift( ifft2( smear_kernal_k .* fft2(TM_pic) ) );
+        TM_pic = ( ifft2( smear_kernal_k .* fft2(TM_pic) ) );
     elseif filter_flag == 3 % phase vortex
         shift_angle = -atan(KY./KX)+pi/2;
         shift_angle(:,1:(sqrt(n_ccd)/2)) = shift_angle(:,1:(sqrt(n_ccd)/2)) + pi;
@@ -31,6 +35,11 @@ for idx_slm = 1:n_slm
         smear_kernal_k((sqrt(n_ccd)/2+1),(sqrt(n_ccd)/2+1)) = 0;
         smear_kernal_k = fftshift(smear_kernal_k);
         TM_pic = fftshift( ifft2( smear_kernal_k .* fft2(TM_pic) ) );
+    elseif filter_flag == 4 % single k-pixel   
+        smear_kernal_k = zeros(size(TM_pic));
+        smear_kernal_k(k==filter_width,k==filter_width) = 1;
+        smear_kernal_k = fftshift(smear_kernal_k);
+        TM_pic = ( ifft2( smear_kernal_k .* fft2(TM_pic) ) );
     end
 
     TM_pic_fft = TM_pic_fft + abs(fftshift(fft2(TM_pic)))/n_slm;
