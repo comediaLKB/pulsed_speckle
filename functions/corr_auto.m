b94a8704d0ebe0490b7147d333c15850c2710d5b
Bernhard Rauer <bernhard.rauer@gmail.com>
2021-03-17 11:18:45 +0100

    changed to abs definition (not sure why anymore)

--- functions/corr_auto.m
+++ functions/corr_auto.m
@@ -1,7 +1,8 @@
 function out = corr_auto(image)
 
     [n, m] = size(image);
-    out = ( fftshift( ifft2(fft2(image) .* conj(fft2(image)) ) ) )./(n*m);
+    %out = ( fftshift( ifft2(fft2(image) .* conj(fft2(image)) ) ) )./(n*m);
+    out = ( fftshift( ifft2(abs(fft2(image)) ) ) )./(n*m);
 
     out = ( out - mean(image(:)).^2 ) / mean(image(:)).^2;
     
